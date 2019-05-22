22c5105d45e216d7e42e7f1f03100adac89ac220
Tai Dang <tai.dangit@gmail.com>
2019-05-22 14:41:22 -0700

    scaffolding complete

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
+const logger = require('morgan');
 
 module.exports = {
   
@@ -10,8 +12,9 @@ module.exports = {
   init(app, express){
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(express.static(path.join(__dirname, "..", "assets")));
-
+    app.use(logger('dev'));
   }
 
 
