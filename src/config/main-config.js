22fa240ef0b98fe1a651039ef5a39a77e1613f16
Tai Dang <tai.dangit@gmail.com>
2019-05-23 01:13:26 -0700

    blocipedia / scaffolding / complete

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
 const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
 const logger = require('morgan');
+const passportConfig = require("./passport-config");
+const session = require("express-session");
+const flash = require("express-flash");
 
 module.exports = {
   
@@ -13,8 +17,22 @@ module.exports = {
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(expressValidator());
     app.use(express.static(path.join(__dirname, "..", "assets")));
     app.use(logger('dev'));
+    app.use(session({
+      secret: process.env.cookieSecret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 1.21e+9 } //set cookie to expire in 14 days
+    }));
+    app.use(flash());
+    passportConfig.init(app);
+
+    app.use((req,res,next) => {
+      res.locals.currentUser = req.user;
+      next();
+    })
   }
 
 
