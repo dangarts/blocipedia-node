6b75c865e8c79240e89dd7abbc862d1456448eba
Tai Dang <tai.dangit@gmail.com>
2019-06-11 09:32:56 -0700

    cp10-markdown / complete

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -1,5 +1,6 @@
 const wikiQueries = require('../db/queries.wikis.js');
 const Authorizer = require('../policies/wiki');
+const markdown = require('markdown').markdown;
 
 module.exports = {
 
@@ -58,6 +59,7 @@ module.exports = {
       if(err || wiki == null){
         res.redirect(404, "/");
       } else {
+        wiki.body = markdown.toHTML(wiki.body);
         res.render("./wikis/show", {wiki});
       }
     });
